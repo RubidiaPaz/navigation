e7350b065ae41c38263a57288c30f9f17828d1ba
RubidiaPaz <rubigarcia190@gmail.com>
2022-10-24 18:28:12 -0600

    creacion de barra de navegacion

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "../screens/Home";
+import About from "../screens/About";
+import Contact from "../screens/Contact";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={Home} options={{ title: "Home" }} />
+      <Tab.Screen
+        name="contact"
+        component={Contact}
+        options={{ title: "Contact" }}
+      />
+      <Tab.Screen name="about" component={About} options={{ title: "About" }} />
+    </Tab.Navigator>
+  );
+}
